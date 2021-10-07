1441f53226f5de5b02c99106c3311e162c68368b
tsunamiyama <tsuchiyamakris@gmail.com>
2021-10-07 14:40:35 -0700

    Updated Hero Animations/Colors/Size

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
-function changeArrowColor() {
-    var line3 = document.getElementById("Line_3");
-    var line4 = document.getElementById("Line_4");
-    var line5 = document.getElementById("Line_3-2");
-    var line6 = document.getElementById("Line_4-2");
-    var line7 = document.getElementById("Line_3-3");
-    var line8 = document.getElementById("Line_4-3");
+function mazeLight() {
+    var col1 = document.getElementById("col1");
+    var col2 = document.getElementById("col2");
+    var col3 = document.getElementById("col3");
+    var col4 = document.getElementById("col4");
+    var col5 = document.getElementById("col5");
+    var col6 = document.getElementById("col6");
+    var col7 = document.getElementById("col7");
+    var col8 = document.getElementById("col8");
+    var col9 = document.getElementById("col9");
+    var col10 = document.getElementById("col10");
+    var col11 = document.getElementById("col11");
+    var col12 = document.getElementById("col12");
+    var col13 = document.getElementById("col13");
 
-    if(line3.style.stroke == "rgb(187, 188, 188)"){
-        line3.style.stroke = "#000";
-        line4.style.stroke = "#000";
-        line5.style.stroke = "#000";
-        line6.style.stroke = "#000";
-        line7.style.stroke = "#000";
-        line8.style.stroke = "#000";
-    } else{
-        line3.style.stroke = "#bbbcbc";
-        line4.style.stroke = "#bbbcbc";
-        line5.style.stroke = "#bbbcbc";
-        line6.style.stroke = "#bbbcbc";
-        line7.style.stroke = "#bbbcbc";
-        line8.style.stroke = "#bbbcbc";
-    }
+    var row1 = document.getElementById("row1");
+    var row2 = document.getElementById("row2");
+    var row3 = document.getElementById("row3");
+    var row4 = document.getElementById("row4");
+    var row5 = document.getElementById("row5");
+    var row6 = document.getElementById("row6");
+    var row7 = document.getElementById("row7");
+
+    
 }
 
-setInterval(function () {changeArrowColor();}, 2000);
+//setInterval(function () {changeArrowColor();}, 2000);
